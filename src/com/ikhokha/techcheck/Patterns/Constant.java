47c2728ed91717f46aa9b49850ff17e5f30badc1
Jean Tsheta <tshieta@gmail.com>
2022-09-22 23:27:33 +0200

    implement of metrics

--- src/com/ikhokha/techcheck/Patterns/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.ikhokha.techcheck.Patterns;
-
-public enum Constant {
-    SHORTER_THAN_15,
-    MOVER_MENTIONS,
-    SHAKER_MENTIONS,
-    QUESTIONS,
-    SPAM
-}
--- src/com/ikhokha/techcheck/Patterns/iMetrics.java
+++
@@ -1,7 +0,0 @@
-package com.ikhokha.techcheck.Patterns;
-
-import java.io.IOException;
-
-public interface iMetrics {
-    public boolean match(String word) throws IOException;
-}
